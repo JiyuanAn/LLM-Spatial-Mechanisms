f4929b15e9a464aa862492f06dd009f7100e59d8
JiyuanAn <jiyuanan.blcu@gmail.com>
2026-01-04 19:38:45 +0800

    Reorganize the code for the linear probe section and optimize the data generation boundary constraints. Organize project directory.

--- src/probe/run.sh
+++
@@ -1,4 +0,0 @@
-python ./layer_sweep_probe_new.py \
-    -m "Qwen/Qwen2.5-7B-Instruct" \
-    -tr "/home/s202507009/workspace/SA-of-LLM/SA-of-LLM/src/data_generation/spatial_reasoning_dataset_EN.json" \
-    -te "/home/s202507009/workspace/SA-of-LLM/SA-of-LLM/src/data_generation/spatial_reasoning_dataset_EN_test.json"
