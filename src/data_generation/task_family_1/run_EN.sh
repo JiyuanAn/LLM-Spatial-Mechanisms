b60a6311f8e3db353fc33df8c3e22dea19bed808
JiyuanAn <jiyuanan.blcu@gmail.com>
2026-01-04 19:53:59 +0800

    Reorganize the code for the linear probe section and optimize the data generation boundary constraints. Organize project directory.

---
+++ src/data_generation/task_family_1/run_EN.sh
@@ -0,0 +1,5 @@
+python ./generate_relation_EN.py
+python ./generate_relation_EN_test.py
+python ./add_prompt_relation_EN.py
+python ./call_inference_api_relation_EN.py
+python ./analyze_results_relation_EN.py
